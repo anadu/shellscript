9de56ae73fa6cbab168f2c98502540d8972f5aff
manoj <ankireddy828@gmail.com>
2023-08-16 20:36:07 +0530

    somechange

--- array.sh
+++ array.sh
@@ -2,5 +2,5 @@
 
 array=("kishore" 20 "manoj")
 
-echo "the first index is $array[0]"
-echo "the second element is $array[#]"
+echo "the first index is ${array[0]}"
+echo "the second element is ${array[@]}"

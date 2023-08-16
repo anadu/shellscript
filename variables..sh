6e56e5aa36c76c917d352d6b8d09f0cad003f5ae
manoj <ankireddy828@gmail.com>
2023-08-16 19:28:22 +0530

    command line argunments

---
+++ variables..sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+#she bangs means you should inform to linux server ,how to excute the script
+
+kishore=$1
+manoj=$2
+
+echo "$kishore is doing great job"
+echo "$manoj is doing excellent job"

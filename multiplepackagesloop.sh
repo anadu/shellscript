8889ab74559dd7a2827780fc37c9712361d558b2
manoj <ankireddy828@gmail.com>
2023-08-17 14:29:45 +0530

    super

--- multiplepackagesloop.sh
+++ multiplepackagesloop.sh
@@ -1,14 +1,12 @@
 validate(){
-    if [ $1 -ne 0 ]
-    then
-       echo "$2 -- failure"
+    if [ $1 -ne 0 ]; then
+        echo "$2 -- failure"
     else
-       echo  "$2  --sucess" 
+        echo "$2 -- success"
+    fi
 }
 
-
-for i in $@
-do
-   yum install $i -y
-   validate $? $i
-done   
+for i in "$@"; do
+    yum install "$i" -y
+    validate $? "$i"
+done

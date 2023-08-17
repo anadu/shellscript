b785c757e409125943d746dca50b9d255f1f5a55
manoj <ankireddy828@gmail.com>
2023-08-17 09:59:11 +0530

    founctionbase

--- founction.sh
+++ founction.sh
@@ -1,33 +1,12 @@
-VALIDATE(){
-    #$1 --> it will receive the argument1
-    if [ $1 -ne 0 ]
-    then
-        echo -e "$2 ... $R FAILURE $N"
-        exit 1
-    else
-        echo -e "$2 ... $G SUCCESS $N"
-    fi
-}
-
 USERID=$(id -u)
+if [ $USERID -ne 0 ]
+then 
+   echo "please run the script to run the sudo user"
+   exit 1
 
-if [ $USERID -eq 0 ]
-then
-    echo "ERROR:: Please run this script with root access"
-    echo "the number of argunemnts $#"
-    echo "the number of variables $@"
-    exit 1
-# else
-#     echo "INFO:: You are root user"
-fi
+yum install git -y
+echo $?
 
-# it is our responsibility again to check installation is success or not
-yum install mysql -y &>>$LOGFILE
+yum install mysql -y
 
-VALIDATE $? "Installing MySQL"
-echo "the number of argunemnts $#"
-echo "the number of variables $@"
-
-yum install posddtfix -y &>>$LOGFILE
-
-VALIDATE $? "Installing postfix"
+echo $?

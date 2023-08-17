7b437211a7f941474cd9fb4b44323f4243b7fc1d
manoj <ankireddy828@gmail.com>
2023-08-17 14:27:00 +0530

    super

--- multiplepackagesloop.sh
+++ multiplepackagesloop.sh
@@ -1,4 +1,14 @@
+validate(){
+    if [ $1 -ne 0 ]
+    then
+       echo "$2 -- failure"
+    else
+       echo  "$2  --sucess" 
+}
+
+
 for i in $@
 do
    yum install $i -y
+   validate $? $i
 done   

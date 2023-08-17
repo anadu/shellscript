a476a29b4d412dce1f6fc08604c329de1479bc97
manoj <ankireddy828@gmail.com>
2023-08-17 12:38:04 +0530

    some message

--- cheeckfountion.sh
+++ cheeckfountion.sh
@@ -8,3 +8,19 @@ then
 fi
 
 yum install git -y
+
+if [ $? -ne 0 ]
+then 
+   echo "git sucess"
+else
+   echo "git failure"
+fi   
+
+yum install mysql -y
+if [ $? -ne 0 ]
+then 
+   echo "mysql sucess"
+else
+   echo " git failure"
+fi   
+

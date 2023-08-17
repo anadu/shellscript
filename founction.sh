93327527945d4c1b8e65b3896b7e5acec8f28e31
manoj <ankireddy828@gmail.com>
2023-08-17 09:38:03 +0530

    founctionmodule

---
+++ founction.sh
@@ -0,0 +1,31 @@
+VALIDATE(){
+    #$1 --> it will receive the argument1
+    if [ $1 -ne 0 ]
+    then
+        echo -e "$2 ... $R FAILURE $N"
+        exit 1
+    else
+        echo -e "$2 ... $G SUCCESS $N"
+    fi
+}
+
+USERID=$(id -u)
+
+if [ $USERID -ne 0 ]
+then
+    echo "ERROR:: Please run this script with root access"
+    exit 1
+# else
+#     echo "INFO:: You are root user"
+fi
+
+# it is our responsibility again to check installation is success or not
+yum install mysql -y &>>$LOGFILE
+
+VALIDATE $? "Installing MySQL"
+echo "the number of argunemnts $#"
+echo " the number of variables $@"
+
+yum install posddtfix -y &>>$LOGFILE
+
+VALIDATE $? "Installing postfix"

60be4bcbed26df18ec2f50b0b44e08235235eeb5
manoj <ankireddy828@gmail.com>
2023-08-18 10:03:52 +0530

    super

--- loop.sh
+++ loop.sh
@@ -4,3 +4,23 @@ for i in {1..100}
 do
    echo $i
 done   
+==================
+
+validate(){
+    if [ $1 -ne 0 ]
+    then
+       echo failure $2
+    else
+       echo sucess $2
+    fi      
+
+}
+
+
+
+for i in $@
+do 
+   yum install $i -y
+   validate $? "$i"
+
+done
